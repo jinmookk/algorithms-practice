63af9b4067bacf13c9bada879d455a23b4231de9
jinmookk <39418254+jinmookk@users.noreply.github.com>
2021-09-24 23:20:16 +0900

    Commit

---
+++ BOJ/Level01/Level01/10171.c
@@ -0,0 +1,24 @@
+/*
+	문제 10171 (출처:https://www.acmicpc.net/problem/10171)
+	아래 예제와 같이 고양이를 출력하시오.
+
+	출력:
+	고양이를 출력한다.
+
+	예제 출력:
+	\    /\
+	)  ( ')
+	(  /  )
+	 \(__)|
+*/
+#include <stdio.h>
+
+int main()
+{
+	printf("\\    /\\ \n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|");
+
+	return 0;
+}
---
+++ BOJ/Level01/Level01/2557.c
@@ -0,0 +1,18 @@
+/*
+    문제 2557 (출처:https://www.acmicpc.net/problem/2557)
+    Hello World!를 출력하시오.
+
+    출력:
+    Hello World!를 출력하시오.
+
+    예제 출력:
+    Hello World!
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
